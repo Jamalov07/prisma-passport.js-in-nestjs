6a4cbd9fb9cde4c8de0791bb794e594fb90e2680
Jamalov <Jamalovn07@gmail.com>
2022-12-02 09:20:29 +0500

    second commit and prisma is added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 3333;
+    const app = await NestFactory.create(AppModule);
+    // app.useGlobalPipes(new ValidationPipe());
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+    await app.listen(PORT, () => {
+      console.log(`Server ${PORT}  portida ishga tushdi`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
